2de12a9cb0dcf20e65553f92c9d7e7ab437f05cf
AbdAllrhman-odeh <137071364+AbdAllrhman-odeh@users.noreply.github.com>
2023-10-24 16:20:57 +0300

    Add files via upload

---
+++ task3/index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>
+        Info about me:
+    </h1>
+    <p>
+    hello, iam ABDALLRAHMAN ,iam studing computer science at just university, iam in my final year,
+        in my fourth year,i wanna be a fullStack developer.
+    </p>
+    <h6>
+        My name is ABDALLRAHMAN ODEH
+    </h6>
+    <img src="a.png" alt="a picture of me" width="200px" height="200px"><br>
+    <img src="vector.jpg" alt="vector stack" width="200px"><br>
+    <hr>
+    <h6>
+        social media sites:
+    </h6>
+    <a href="https://www.facebook.com/" target="_blank" title="click to go to facebook">facebook</a><br>
+    <a href="https://www.instagram.com/" target="_blank" title="click to go to instagram">instagram</a>
+    <br>
+    <hr>
+    <h6>
+        Favirote animal:
+    </h6>
+    <ul>
+        <li>
+            lion
+        </li>
+        <li>
+            cat
+        </li>
+        <li>
+            dog
+        </li>
+    </ul>
+    <hr>
+    <h6>
+        my experinces
+    </h6>
+    hello, iam ABDALLRAHMAN ,iam studing computer science at just university, iam in my final year,
+        in my fourth year,i wanna be a fullStack developer.
+    <hr>
+</body>
+</html>
